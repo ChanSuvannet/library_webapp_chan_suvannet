7e06571320bbdae38f9211696b61775d9d9c2bd6
CHAN Suvannet <chansuvannet999@gmail.com>
2025-05-09 10:08:59 +0700

    first commit

---
+++ src/main/java/gic/i4/library_webapp_chan_suvannet/LibraryWebappChanSuvannetApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_chan_suvannet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappChanSuvannetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappChanSuvannetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_chan_suvannet/LibraryWebappChanSuvannetApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_chan_suvannet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappChanSuvannetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
